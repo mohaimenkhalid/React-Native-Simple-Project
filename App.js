c309b3efd671a2e336462e7793138c11aaa39917
Abdul Mohaimen <mohaimen707@gmail.com>
2021-10-30 23:41:40 +0600

    Ta navigator item adds

--- App.js
+++ App.js
@@ -19,6 +19,8 @@ import {request, PERMISSIONS} from 'react-native-permissions';
 import { NavigationContainer, useNavigation } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+
 
 
 const Link = () => {
@@ -32,14 +34,33 @@ const Link = () => {
 const Tab = createBottomTabNavigator();
 function MyTabs() {
   return (
-    <Tab.Navigator>
-      <Tab.Screen name="Home1" component={StackNavigator}
-        options={{
-          headerShown: false
-         }}
+    <Tab.Navigator
+      screenOptions={{
+        tabBarActiveTintColor: 'tomato',
+          tabBarInactiveTintColor: 'gray',
+      }}
+    >
+      <Tab.Screen 
+      name="Home1" 
+      component={StackNavigator}
+      options={{
+        headerShown: false,
+        tabBarIcon: ({size, color}) => <MaterialCommunityIcons name="home" color={color} size={size} />
+        }}
       />
       <Tab.Screen name="Settings"
        component={MessageScreen}
+       options={{
+      
+        tabBarIcon: ({size, color}) => <MaterialCommunityIcons name="email" color={color} size={size} />
+        }}
+       />
+       <Tab.Screen name="Message"
+       component={MessageScreen}
+       options={{
+      
+        tabBarIcon: ({size, color}) => <MaterialCommunityIcons name="email" color={color} size={size} />
+        }}
        />
     </Tab.Navigator>
   );
